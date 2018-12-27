62bb012f3eb9a7b48a9bca626e26f22a1d6da41a
BeulahWorks <35774412+BeulahWorks@users.noreply.github.com>
2018-12-27 08:04:53 -0600

    Update shell_connection.R

--- R/shell_connection.R
+++ R/shell_connection.R
@@ -295,7 +295,7 @@ start_shell <- function(master,
     shell_args <- c(shell_args, "--conf", paste0("spark.yarn.am.waitTime=",config[["spark.yarn.am.waitTime"]]))
     shell_args <- c(shell_args, "--conf", paste0("spark.driver.port=",config[["spark.driver.port"]]))
     shell_args <- c(shell_args, "--conf", paste0("spark.blockManager.port=",config[["spark.blockManager.port"]]))
-    
+    shell_args <- c(shell_args, "--conf", paste0("spark.history.ui.port=",config[["spark.history.ui.port"]]))
     
     # add environment parameters to arguments
     shell_env_args <- Sys.getenv("sparklyr.shell.args")
